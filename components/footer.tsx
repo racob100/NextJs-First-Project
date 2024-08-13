38784eefc2811a93dd010bb496f9429fbcc011e9
racob100 <mrityunjoykdeka100@gmail.com>
2024-08-13 13:58:02 +0530

    Made the social links clickable

--- components/footer.tsx
+++ components/footer.tsx
@@ -8,9 +8,17 @@ function Footer() {
         <Image src="/Frame 28.png" alt="Logo" width={165} height={100} />
         <div className="mt-4 sm:mt-0">
           <div className="text-slate-500">Socials</div>
-          <div>X/Twitter</div>
-          <div>Discord</div>
-          <div>LinkedIn</div>
+          <div className="flex flex-col space-y-2">
+            <a href="#" target="_blank" rel="noopener noreferrer" className="hover:underline">
+              X/Twitter
+            </a>
+            <a href="#" target="_blank" rel="noopener noreferrer" className="hover:underline">
+              Discord
+            </a>
+            <a href="#" target="_blank" rel="noopener noreferrer" className="hover:underline">
+              LinkedIn
+            </a>
+          </div>
         </div>
       </div>
     </footer>
